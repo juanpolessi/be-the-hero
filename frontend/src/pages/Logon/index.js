5805a5efd0342a6cb53a9c2b71191f61d4bef4bf
Juan Ruiz <polessijuan@gmail.com>
2020-04-29 23:15:28 -0300

    Front-end do App

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
-export default function Logon(){
+export default function Logon() {
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            const response = api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', (await response).data.name);
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Falha no login. Tente novamente.')
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
-                <img src={logoImg} alt="Be The Hero"/>
+                <img src={logoImg} alt="Be The Hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
-                        <FiLogIn size={16} color="#E02041"/>
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
-            <img src={heroesImg} alt="Heroes"/>
+            <img src={heroesImg} alt="Heroes" />
         </div>
     );
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Logon from './pages/Logon';
+import Logon from './pages/logon';
+import Register from './pages/register';
+import Profile from './pages/profile';
+import NewIncident from './pages/newIncident';
 
-export default function Routes () {
+export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Logon} />
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     );
